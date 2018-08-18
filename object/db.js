2d600c4bd5b9819a500db36afe9b6a585dc206e7
toanva <vuanhtoan17@gmail.com>
2018-08-18 08:40:17 +0700

    23
    23

---
+++ object/db.js
@@ -0,0 +1,3 @@
+module.exports = {
+    url: "mongodb://nosavnnet:nsvn6688@nosavn.net:27018/Nosa_Write_About_Teachers"
+};
--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 const { MessengerClient } = require('messaging-api-messenger');
 var config = require('config');
+var objDb = require('./object/database.js');
 
 // App Secret can be retrieved from the App Dashboard
 const APP_SECRET = (process.env.MESSENGER_APP_SECRET) ?
@@ -16,7 +17,11 @@ const VALIDATION_TOKEN = (process.env.MESSENGER_VALIDATION_TOKEN) ?
 // Generate a page access token for your page from the App Dashboard
 const PAGE_ACCESS_TOKEN = (process.env.MESSENGER_PAGE_ACCESS_TOKEN) ?
 	(process.env.MESSENGER_PAGE_ACCESS_TOKEN) :
-	config.get('pageAccessToken');
+    config.get('pageAccessToken');
+
+const SERVER_URL = (process.env.SERVER_URL) ?
+    (process.env.SERVER_URL) :
+    config.get('serverURL');
 
 //const client = MessengerClient.connect();
 const client = MessengerClient.connect({
@@ -29,10 +34,81 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-//server.get('/senddocument', (req, res, next) => {
-//    res.sendFile('views/send.html', {
-//        root: __dirname
+server.get('/senddocument', (req, res, next) => {
+    let referer = req.get('Referer');
+    //console.log("register.bot 0",referer);
+    if (referer) {
+        if (referer.indexOf('www.messenger.com') >= 0) {
+            console.log("register.bot 1", referer);
+            res.setHeader('X-Frame-Options', 'ALLOW-FROM https://www.messenger.com/');
+        } else if (referer.indexOf('www.facebook.com') >= 0) {
+            console.log("register.bot 2", referer);
+            res.setHeader('X-Frame-Options', 'ALLOW-FROM https://www.facebook.com/');
+        } else if (referer.indexOf('staticxx.facebook.com') >= 0) {
+            console.log("register.bot 3", referer);
+            res.setHeader('X-Frame-Options', 'ALLOW-FROM https://staticxx.facebook.com');
+        }
+        req.session.faceUser = true;
+        //res.render('register');
+        res.sendFile('views/senddocument.html', {
+            root: __dirname
+        });
+    }
+});
+//server.get('/registerspostback.bot', authFace, (req, res) => {
+//    let body = req.query;
+
+//    //console.log("body : ",body);
+//    //var dateBrithDay= body.txtBrithDay;
+//    res.status(200).send('Please close this window to return to the conversation thread.');
+//    var returnMessage = "Cảm ơn bạn đã cung cấp thông tin. Nosa kiểm tra lại nhé: Bạn tên là " + body.txtFullName + ", sinh ngày : " + body.txtDay + "/" + body.txtMonth + "/" + body.txtYear + " . Hiện bạn đang giữ chức vụ " + body.cboPosition + ", địa chỉ : " + body.txtWards + " , quận / huyện " + body.txtDistricts + ", Tỉnh / TP " + body.txtProvincial + " . Số điện thoại của bạn là : " + body.txtPhone + " Chuẩn chưa nhỉ?";
+//    //console.log(returnMessage);
+//    var mydate = new Date(parseInt(body.txtYear), parseInt(body.txtMonth) - 1, parseInt(body.txtDay));
+//    //console.log("Date", mydate);
+//    req.session.psid = body.psid;
+//    var inputDate = new Date(mydate.toISOString());
+//    var objMember = {
+//        "_id": body.psid,
+//        "Name": body.txtFullName,
+//        "Birthday": inputDate,
+//        "Provincial": body.txtProvincial,
+//        "District": body.txtDistricts,
+//        "Position": body.cboPosition,
+//        "Ward": body.txtWards,
+//        "Phone": body.txtPhone,
+//        "BlockStatus": "Active"
+//    };
+//    if (objMember.District == undefined)
+//        objMember.District = 'NA';
+//    if (objMember.Ward == undefined)
+//        objMember.Ward = 'NA';
+//    var query = {
+//        "Name": objMember.Provincial
+//    };
+//    objDb.getConnection(function (client) {
+//        objDb.findProvincial(query, client, function (results) {
+//            if (results.length == 1) {
+//                objMember.GeoCodeProvincial = results[0].GeoCode;
+//            } else {
+//                objMember.GeoCodeProvincial = 'NA';
+//            }
+//            objDb.insertMembers(objMember, client, function (err, results) {
+//                //	   res.send(results);
+//                //console.log(results);
+//                if (err) {
+//                    sendTextMessage(body.psid, 'Echo:' + err);
+//                } else {
+//                    client.close();
+//                    sendBackRegister(body.psid, returnMessage);
+//                }
+
+//                //// enc insert member
+//            });
+//            ////  end  findProvincial
+//        });
+//        /// end con
 //    });
+
 //});
 
 router.get('/webhook', function(req, res, next) {
@@ -92,12 +168,59 @@ router.post('/webhook', function(req, res, next) {
 		// Returns a '404 Not Found' if event is not from a page subscription
 		res.sendStatus(404);
 	}
-	
-	
 });
 
+function sendRegisterForm(recipientId, msg) {
+    var messageData = {
+        recipient: {
+            id: recipientId
+        },
+        message: {
+            attachment: {
+                type: "template",
+                payload: {
+                    template_type: "button",
+                    text: msg,
+                    buttons: [{
+                        type: "web_url",
+                        url: SERVER_URL + "/senddocument",
+                        title: "Gửi bài viết",
+                        messenger_extensions: true,
+                        webview_height_ratio: "tall",
+                        fallback_url: SERVER_URL + "/senddocument"
+                    }]
+                }
+            }
+        }
+    };
+    callSendAPI(messageData);
+};
 
-
+function callSendAPI(messageData) {
+    request({
+        uri: 'https://graph.facebook.com/v3.1/me/messages',
+        qs: {
+            access_token: PAGE_ACCESS_TOKEN
+        },
+        method: 'POST',
+        json: messageData
+    },
+        function (error, response, body) {
+            if (!error && response.statusCode == 200) {
+                var recipientId = body.recipient_id;
+                var messageId = body.message_id;
+                if (messageId) {
+                    console.log("Successfully sent message with id %s to recipient %s",
+                        messageId, recipientId);
+                } else {
+                    console.log("Successfully called Send API for recipient %s",
+                        recipientId);
+                }
+            } else {
+                console.error(response.error);
+            }
+        });
+};
 function receivedMessage(event) {
 	var senderID = event.sender.id;
 	var recipientID = event.recipient.id;
@@ -126,8 +249,7 @@ function receivedMessage(event) {
 		client.sendText(senderID, 'Hello! quickReply', { tag: 'ISSUE_RESOLUTION' });
 	}else if (messageText) {
 		switch (messageText.toLowerCase()) {
-			case 'giá xe':
-				client.sendMessage(senderID, {  text: 'Hanoi (KV1) -> Noibai: 200k,Noibai -> Hanoi (KV1): 250k',});
+            case 'Thông tin chương trình':
 				client.sendAttachment(senderID, {
 				  type: 'image',
 				  payload: {
@@ -135,7 +257,13 @@ function receivedMessage(event) {
 				  },
 				});
 				break;
-			case 'liên hệ':
+            case 'Gửi bài viết':
+                client.sendAttachment(senderID, {
+                    type: 'image',
+                    payload: {
+                        url: 'https://scontent.fhan3-2.fna.fbcdn.net/v/t1.0-9/31081528_568961726811775_3035050846015455232_n.jpg?_nc_cat=0&oh=275c0f15fc0d56e03fee30afc9bea818&oe=5C060612',
+                    },
+                });
 				break;
 			default:
                 client.getUserProfile(senderID).then(user => {
@@ -161,13 +289,6 @@ function receivedMessage(event) {
                             type: 'web_url',
                             url: 'http://www.chiasecungthayco.com/2018',
                             title: 'Bình chọn'
-                        }
-                    ]);
-                    client.sendButtonTemplate(senderID,'', [
-                        {
-                            type: 'web_url',
-                            url: 'https://toanvachatbot.herokuapp.com/document.html',
-                            title: 'Bài viết chọn lọc'
                         }
                     ]);
                 });
